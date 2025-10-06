cbfc96fa48f945b607911d9ed36f5ee51d9acae1
kirillfedulin <crazzyhd22@gmail.com>
2025-10-06 18:07:01 +0300

    5 ulesanne

---
+++ ulesanded.py
@@ -0,0 +1,73 @@
+
+#esimene ülesanne
+#Kirjuta enda esimene programm, mis väljastab käsureale teksti: “Tere, maailm!”. 
+#Küsi kasutaja nimi ja muuda tekst, et ta näeks välja nii: “Tere, maailm! Tervitan sind Mati”, kui kasutaja nimi on Mati.
+#Küsi kasutajalt sisend tema vanuse kohta ning väljasta see ekraanile:
+#“Tere, maailm! Tervitan sind Mati! Sa oled N aastat vana.”
+
+import random
+import math
+
+
+
+#print("Hello World!")
+#name = str(input("What is ur name?"))
+#print(f"Hello World! Hello {name.upper()}!")
+#age = int(input("What is ur age?"))
+#print(f"Hello World! Hello {name.upper()}!{name.upper()} are {age} years old!")
+
+
+
+
+#Mis tüüpi on järgnevad muutujad:
+#a) vanus = 18
+#b) eesnimi = "Jaak"
+#c) pikkus = 16.5
+#d) kas_käib_koolis = True
+#Mis võimalus veel peale True oleks viimast muutujat väärtustada? Kuidas võiks nende muutujate väärtusi koodis kontrollida?
+#Kirjuta kood tüüpide kontrollimiseks.
+
+
+#vanus = int(input("Enter ut age: "))
+#eesnimi = str(input("Enter ur name: "))
+#pikkus = float(input("Enter ur tall"))
+#kas_käib_koolis = bool(input("Do u go to school?"))
+
+
+
+
+#Kirjuta enda koodis laual olevate kommide arv muutujasse(kommide arv on juhuslik). Seejärel kuva muutujas olev kommide arv ekraanile kasutades print() käsku.
+#Küsi kasutajalt sisendit, mitu kommi ta soovib laualt ära võtta. Eemalda soovitud kommide arv laual olevate kommide arvust ja kuva ekraanile, kui palju komme laual nüüd on. 
+
+#candy = random.randint(1, 100)
+#print(f"We have {candy} candys")
+# = int(input("How much candys u want?"))
+#end = candy-want
+#if end < 0:
+#    print("We cant do this")
+#else:
+#    print(f"U get {end} candys")
+#end
+
+
+
+
+#Puu läbimõõdu arvutamine
+#Kirjuta programm, mis küsib kasutaja käest puu ümbermõõdu ning teatab selle peale puu läbimõõdu.
+#ask = int(input("Write circle: "))
+#diametor = 2*ask
+#L = diametor/3.14
+#print(L)
+
+
+
+
+
+#Arvutage Pythoni käsureal, kui pikk on ristkülikukujulise maatüki diagonaal, mille mõõtmed on Nm x Mm. N ja M küsi kasutajalt
+#N = int(input("Enter first num: "))
+#M = int(input("Enter second num: "))
+#D = math.isqrt(N*N + M*M)
+#print(D)
+
+
+
